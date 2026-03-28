1e19d309a3038249c15d7a2749976aafa00c84e0
gerritq <g.quaremba@gmail.com>
2026-03-28 13:39:44 +0000

    task 2 results in out; last 500 missing

--- main.sh
+++ main.sh
@@ -2,19 +2,19 @@
 #SBATCH --job-name=argmin_100
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
-#SBATCH --time=01:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=25GB
-# SBATCH --constraint=a100
+#SBATCH --constraint=h200|b200
 
 nvidia-smi
 
 set -euo pipefail
 
-MODEL_ID="Qwen/Qwen3-0.6B" # Qwen/Qwen3-8B Qwen/Qwen3-0.6B
-SMOKE_TEST=1
-N=5
+MODEL_ID="Qwen/Qwen3-4B" # Qwen/Qwen3-8B Qwen/Qwen3-0.6B Qwen/Qwen3-4B
+SMOKE_TEST=0
+N=3000
 MODE="single"
 
 uv run main.py \
