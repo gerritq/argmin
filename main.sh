5c036f057bbfe437476c7f6bf80cbf4165f143c5
gerritq <g.quaremba@gmail.com>
2026-03-30 09:34:31 +0100

    final run with 8b

--- main.sh
+++ main.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=argmin_100
+#SBATCH --job-name=argmin_3000
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
-#SBATCH --time=04:00:00
+#SBATCH --time=12:00:00
 #SBATCH --partition=gpu,nmes_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=25GB
-#SBATCH --constraint=h200|b200
+#SBATCH --constraint=h200|b200|a100
+#SBATCH --exclude=erc-hpc-comp034
 
 nvidia-smi
 
 set -euo pipefail
 
-MODEL_ID="Qwen/Qwen3-4B" # Qwen/Qwen3-8B Qwen/Qwen3-0.6B Qwen/Qwen3-4B
+MODEL_ID="Qwen/Qwen3-8B" # Qwen/Qwen3-8B Qwen/Qwen3-0.6B Qwen/Qwen3-4B
 SMOKE_TEST=0
-N=3000
+N=500
 START_N=2500
 MODE="single"
 
